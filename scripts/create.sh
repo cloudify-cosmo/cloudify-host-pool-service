55c5deb4c1064677bc3ddda7de6e7657015f40d2
iliapolo <elip@gigaspaces.com>
2015-04-19 17:13:51 +0300

    CFY-2209 dont create seperate virtualenv but rather use the agent one

--- scripts/create.sh
+++ scripts/create.sh
@@ -4,9 +4,7 @@ SOURCE=$(ctx node properties source)
 
 directory=$(ctx node properties directory)
 
-work_directory=${directory}/work
-
-env_directory=${directory}/env
+work_directory=${directory}
 
 ctx logger info "Creating directory ${directory}"
 mkdir -p ${directory}
@@ -14,14 +12,11 @@ cd ${directory}
 
 mkdir -p ${work_directory}
 
-ctx logger info "Creating virtualenv"
-virtualenv ${env_directory}
-. ${env_directory}/bin/activate
-
 ctx logger info "Installing gunicorn"
-pip install gunicorn
+pip install gunicorn==18.0
+ctx logger info "Installing pyyaml"
+pip install pyyaml==3.10
 ctx logger info "Installing cloudify-host-pool-service"
 pip install ${SOURCE}
 
 ctx instance runtime-properties work_directory ${work_directory}
-ctx instance runtime-properties env_directory ${env_directory}
